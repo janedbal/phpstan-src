0a004e899d63fb989a90148d4170f74971f20b50
Jan Nedbal <janedbal@gmail.com>
2024-08-30 11:33:33 +0200

    ConstantArrayType: fix returned ConstantArrayTypeAndMethod

--- src/Type/Constant/ConstantArrayType.php
+++ src/Type/Constant/ConstantArrayType.php
@@ -483,6 +483,7 @@ class ConstantArrayType implements Type
 			}
 
 			$method = $typeAndMethodName->getType()
+				->getObjectTypeOrClassStringObjectType()
 				->getMethod($typeAndMethodName->getMethod(), $scope);
 
 			if (!$scope->canCallMethod($method)) {
@@ -567,7 +568,7 @@ class ConstantArrayType implements Type
 				$has = $has->and(TrinaryLogic::createMaybe());
 			}
 
-			$typeAndMethods[] = ConstantArrayTypeAndMethod::createConcrete($type, $method->getValue(), $has);
+			$typeAndMethods[] = ConstantArrayTypeAndMethod::createConcrete($classOrObject, $method->getValue(), $has);
 		}
 
 		return $typeAndMethods;
