9ce26851ef2f77141f3e0a882069c125455ddf5c
Jan Nedbal <janedbal@gmail.com>
2024-12-27 13:29:18 +0100

    Failing test: rule hooked to PhpParser\Name node

---
+++ tests/PHPStan/Rules/NameHookedRuleTest.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules;
+
+use PhpParser\Node;
+use PhpParser\Node\Name;
+use PHPStan\Analyser\Scope;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<Rule>
+ */
+class NameHookedRuleTest extends RuleTestCase
+{
+
+	/**
+	 * @return Rule<Node>
+	 */
+	protected function getRule(): Rule
+	{
+		return new class implements Rule {
+
+			public function getNodeType(): string
+			{
+				return Name::class;
+			}
+
+			/**
+			 * @param Name $node
+			 */
+			public function processNode(Node $node, Scope $scope): array
+			{
+				return RuleErrorBuilder::message('Found a name: ' . $node->toString())
+					->identifier('test.name.hooked')
+					->build();
+			}
+
+		};
+	}
+
+	public function testRule(): void
+	{
+		$this->analyse([__DIR__ . '/data/name-hooked.php'], [
+			[
+				'Found a name: NameHooked',
+				3,
+			],
+		]);
+	}
+
+}
