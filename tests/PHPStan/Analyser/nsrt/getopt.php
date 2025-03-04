98d59901b09109fd5e04e4b2d2985b1f61a5442a
Jan Nedbal <janedbal@gmail.com>
2025-03-04 14:02:32 +0100

    Fix test

--- tests/PHPStan/Analyser/nsrt/getopt.php
+++ tests/PHPStan/Analyser/nsrt/getopt.php
@@ -7,4 +7,4 @@ use function PHPStan\Testing\assertType;
 
 $opts = getopt("ab:c::", ["longopt1", "longopt2:", "longopt3::"], $restIndex);
 assertType('(array<string, list<mixed>|string|false>|false)', $opts);
-assertType('int', $restIndex);
+assertType('int<1, max>', $restIndex);
